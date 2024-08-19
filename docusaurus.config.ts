b05fcafbb8796c22664939c7189e656a567d61d0
Vitor Dias <vitor_dias@msn.com>
2024-08-19 19:24:21 -0300

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,6 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TCDNUmbanda', // Usually your GitHub org/user name.
   projectName: 'tcdn-website', // Usually your repo name.
-  deploymentBranch:"gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
